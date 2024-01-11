13b594101d5d180346d36e8828f82671395ed36e
administrator <administrator@CPQO-MB01.local>
2024-01-11 10:13:26 +0800

    added exported sample data and region support on importData script

--- importData.sh
+++ importData.sh
@@ -2,6 +2,10 @@ echo "**************************************************************************
 echo "Welcome to Dynamodb Data Batch Importer"
 echo "It will import your Dynamodb data to aws using aws cli batch-write-item"
 echo "Before we started, please check your aws configure"
+
+echo "Please enter the region: "
+read -e aws_region_name
+
 echo "Please enter the exported table name"
 read -e folder_path
 echo "finding import script at " $folder_path/ScriptForDataImport
@@ -11,7 +15,7 @@ start_time="$(date -u +%s)"
 #import all data in the folder
 for filename in $folder_path/ScriptForDataImport/*.txt; do
     echo "importing ${filename}"
-    aws dynamodb batch-write-item --request-items file://${filename}
+    aws dynamodb batch-write-item --region $aws_region_name --request-items file://${filename}
 done
 
 
